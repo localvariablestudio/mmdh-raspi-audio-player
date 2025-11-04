488fcd340b03114f0329db7460abbd0c31248a2d
guillemontecinos <gamontecin@gmail.com>
2025-11-04 19:45:15 -0300

    gpio wip

--- scripts/audio-button-control.py
+++ scripts/audio-button-control.py
@@ -3,7 +3,7 @@ from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
     print(GPIO.input(2))
