4da396f4fd08119ac7f0c76644a0d5566c4304f4
guillemontecinos <gamontecin@gmail.com>
2025-11-04 23:13:41 -0300

    gpio wip

--- scripts/audio-button-control.py
+++ scripts/audio-button-control.py
@@ -11,11 +11,13 @@ play_status = False
 # Pins config
 btn1 = [2, 3]
 GPIO.setup(btn1[0], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(btn1[1], GPIO.OUT, initial=GPIO.LOW)
 
 def event_catch(ch):
     global play_status
     play_status = not play_status
     print('Play status: ', play_status)
+    GPIO.output(btn1[1], play_status)
 
 GPIO.add_event_detect(btn1[0], GPIO.FALLING, callback=event_catch, bouncetime=100)
 
