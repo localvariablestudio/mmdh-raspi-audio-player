5a4aeabbe614248cfb456353f99f78d280d60586
guillemontecinos <gamontecin@gmail.com>
2025-11-04 22:45:42 -0300

    gpio wip

--- scripts/audio-button-control.py
+++ scripts/audio-button-control.py
@@ -9,7 +9,7 @@ GPIO.setwarnings(False)
 pin_in = 2
 GPIO.setup(pin_in, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def event_catch():
+def event_catch(ch):
     print('Button down')
 
 GPIO.add_event_detect(pin_in, GPIO.FALLING, callback=event_catch)
