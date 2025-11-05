19d76ef3926628d5d4bdf96bced36598383ee83e
guillemontecinos <gamontecin@gmail.com>
2025-11-04 22:58:29 -0300

    gpio wip

--- scripts/audio-button-control.py
+++ scripts/audio-button-control.py
@@ -5,12 +5,16 @@ from time import sleep
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+# Audio control
+play_status = False
+
 # Pins config
 btn1 = [2, 3]
 GPIO.setup(btn1[0], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def event_catch(ch):
-    print('Button down')
+    play_status = not play_status
+    print('Play status: ', play_status)
 
 GPIO.add_event_detect(btn1[0], GPIO.FALLING, callback=event_catch, bouncetime=100)
 
