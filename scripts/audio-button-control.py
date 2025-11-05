53f4322694e9d0ba8c7f05bf64e96d2ee12f8f11
guillemontecinos <gamontecin@gmail.com>
2025-11-04 22:34:17 -0300

    gpio wip

--- scripts/audio-button-control.py
+++ scripts/audio-button-control.py
@@ -1,9 +1,13 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
+pin_in = 2
+
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(pin_in, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-while True:
-    print(GPIO.input(2))
+def event_catch(channel):
+    print('Button down in ', channel)
+
+GPIO.add_event_detect(pin_in, GPIO.FALLING, callback=event_catch)
