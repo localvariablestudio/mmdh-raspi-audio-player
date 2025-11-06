9b837eb73bd6bb5a722339c08e0c853bc6c4febb
guillemontecinos <gamontecin@gmail.com>
2025-11-05 22:54:52 -0300

    gpio wip

--- scripts/audio-button-control.py
+++ scripts/audio-button-control.py
@@ -21,7 +21,7 @@ def play_audio():
     global play_status
     try:
         # Open the WAV file
-        f = wave.open('../assets/audio-test.wav', 'rb') 
+        f = wave.open('../assets/audio-test-2.wav', 'rb') 
 
         # Initialize a PCM device for playback
         # 'default' refers to the default sound card and device
